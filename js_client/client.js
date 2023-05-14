4740dcd8cfb36bbc470ec605612975dda0287061
onehandedpirate <plagueismkii@gmail.com>
2023-05-14 18:17:00 +1000

    End

--- js_client/client.js
+++ js_client/client.js
@@ -1,8 +1,13 @@
 const loginForm = document.querySelector('#login-form')
+const searchForm = document.querySelector('#search-form')
 const baseEndpoint = "http://localhost:8000/api"
 const contentContainer = document.querySelector('#content-container')
 if (loginForm) {
     loginForm.addEventListener('submit', handleLogin)
+}
+
+if (searchForm) {
+    searchForm.addEventListener('submit', handleSearch)
 }
 
 function handleLogin(event) {
@@ -22,6 +27,51 @@ function handleLogin(event) {
         .then(response => response.json())
         .then(authData => {
             handleAuthData(authData, getProductList)
+        })
+        .catch(
+            err => console.log(err)
+        )
+}
+
+function handleSearch(event) {
+    event.preventDefault()
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    // console.log(data)
+    let searchParams = new URLSearchParams(data)
+    const endpoint = `${baseEndpoint}/search/v2/?${searchParams}`
+    let headers = {
+        "Content-Type": "application/json",
+
+    }
+    const authToken = localStorage.getItem('access')
+    if (authToken) {
+        headers['Authorization'] = `Bearer ${authToken}`
+    }
+    const options = {
+        method: 'GET',
+        headers: headers,
+    }
+    fetch(endpoint, options)
+        .then(response => response.json())
+        .then(data => {
+            const validData = isTokenNotValid(data)
+            if (validData && contentContainer) {
+                contentContainer.innerHTML = ''
+                if (data && data.hits) {
+                    let htmlStr = ''
+                    for (let result of data.hits) {
+                        htmlStr += "<li>" + result.title + "</li>"
+                    }
+                    contentContainer.innerHTML = htmlStr
+                    if (data.hits.length === 0) {
+                        contentContainer.innerHTML = "<p>No results fount</p>"
+                    }
+                } else {
+                    contentContainer.innerHTML = "<p>No results fount</p>"
+                }
+            }
+            // writeToContainer(data)
         })
         .catch(
             err => console.log(err)
@@ -57,6 +107,8 @@ function getFetchOptions(method, body) {
 function isTokenNotValid(jsonData) {
     if (jsonData.code && jsonData.code === 'token_not_valid') {
         alert('Please log in again!')
+    } else {
+        return true
     }
 }
 
@@ -75,3 +127,47 @@ function getProductList() {
             }
         })
 }
+
+
+const searchClient = algoliasearch('O6EQXFR0PU', '04c6af6e6556f875567b18453f9d833c');
+
+const search = instantsearch({
+    indexName: 'cfe_Product',
+    searchClient,
+});
+
+search.addWidgets([
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+    }),
+
+    instantsearch.widgets.clearRefinements({
+        container: '#clear-refinements',
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#user-list',
+        attribute: 'user'
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#public-list',
+        attribute: 'public'
+    }),
+
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            item: `
+            <div>
+                <div>{{ title }}</div>
+                <div>{{ body }}</div>
+                
+                <p>{{ user }}</p><p>\${{ price }}</p>
+            </div>`
+        }
+    })
+]);
+
+search.start();
+
